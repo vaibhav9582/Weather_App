42ebd9d501432971e68cec051b1e1dbe943ad231
vaibhav <vaibhavkummar@gmail.com>
2024-06-05 06:33:38 +0900

    nol

--- app.js
+++ app.js
@@ -16,7 +16,9 @@ const flash = require("connect-flash");
 
 const mongoose = require('mongoose');
 
-main().catch(err => console.log(err));
+main().then(()=>{
+  console.log("ho gya");
+}).catch(err => console.log(err));
 
 async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/testinendgame2');
