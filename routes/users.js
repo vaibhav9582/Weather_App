61b5c6d31c61cc819aac488d92a829c7d9a81701
vaibhav <vaibhavkummar@gmail.com>
2024-06-04 19:12:55 +0530

    lol

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ const passport = require("passport");
 
 const plm = require("passport-local-mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017/testinendgame2");
+mongoose.connect(process.env.MONGO);
 
 const userSchema = mongoose.Schema({
     username : String,
